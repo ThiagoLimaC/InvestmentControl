2c4aa0dc72e030297d53ba305ccb3554d2d49492
Thiago Lima <150944198+ThiagoLimaC@users.noreply.github.com>
2025-06-02 21:52:57 -0300

    Criei a view para adicionar investimentos e a viewmodel para receber dados e enviar requests
    adicionei o using da pasta viewmodels no arquivo ViewImports

---
+++ Controllers/InvestimentoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvestmentControl.Controllers
+{
+    public class InvestimentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+
+        public IActionResult Adicionar(string nome, string tipo, decimal valor, DateTime data)
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewModels/InvestimentoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace InvestmentControl.ViewModels
+{
+    public class InvestimentoViewModel
+    {
+        public int InvestimentoId { get; set; }
+        public string Nome { get; set; }
+        public string Tipo { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime DataInvestimento { get; set; }
+    }
+}
